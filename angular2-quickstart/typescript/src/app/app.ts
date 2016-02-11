00b6d67aafcfbef1126100402c51f40b3a853f68
kenji <kennjimoritata@yahoo.co.jp>
2016-02-11 15:04:31 +0900

    add child Component

--- angular2-quickstart/typescript/src/app/app.ts
+++ angular2-quickstart/typescript/src/app/app.ts
@@ -1,9 +1,17 @@
-import {bootstrap, Component} from 'angular2/angular2';
+import {bootstrap, Component, CORE_DIRECTIVES} from 'angular2/angular2';
+@Component({
+    selector: `hello-world`,
+    template: `<h1>My First Angular 2 App</h1>`,
+})
+class HelloWorld { }
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: `my-app`,
+  template: `
+  <hello-world></hello-world>
+  `,
+  directives: [CORE_DIRECTIVES, HelloWorld]
 })
-class AppComponent { }
-
-bootstrap(AppComponent);
+class MyApp {
+}
+bootstrap(MyApp);
